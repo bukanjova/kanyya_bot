751819f24199547a638478298d50528afe9bc902
Jova Andreas <89806048+bukanjova@users.noreply.github.com>
2021-08-31 19:40:30 +0000

    adjust love

--- temp_data.py
+++ temp_data.py
@@ -15,16 +15,19 @@ async def get_hate():
 
 async def get_love(message):
   msg = message.content
-  slug = msg.split(' ')
-  acc = slug[3] == 'love' and len(slug) == 5
-  if acc:
-    person1 = slug[2]
-    person2 = slug[4][:-1]
-    if person1 == person2:
-      await message.channel.send('invalid input')
-    elif not (len(person1) + len(person2)) % 3:
-      await message.channel.send('yes')
+  if msg.endswith('?'):
+    slug = msg.split(' ')
+    acc = slug[3] == 'love' and len(slug) == 5
+    if acc:
+      person1 = slug[2]
+      person2 = slug[4][:-1]
+      if person1 == person2:
+        await message.channel.send("i don't know")
+      elif not (len(person1) + len(person2)) % 3:
+        await message.channel.send('yes')
+      else:
+        await message.channel.send('no')
     else:
-      await message.channel.send('no')
+      await message.channel.send('invalid input')
   else:
-    await message.channel.send('invalid input')
+    await message.channel.send("missing '?'")
